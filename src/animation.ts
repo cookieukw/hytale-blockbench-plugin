87e65b53e2369b725c4570cc08d720210a67bb5a
JannisX11 <jannis4236@gmail.com>
2025-11-15 23:36:46 +0100

    Smooth quaternion interpolation

--- src/animation.ts
+++ src/animation.ts
@@ -198,3 +198,34 @@ export function setupAnimationActions() {
 	MenuBar.menus.animation.addAction(export_anim);
 	Panels.animations.toolbars[0].add(export_anim, '4');
 }
+
+// Playback
+function weightedCubicBezier(t: number): number {
+	// Control points
+	let P0 = 0.0, P1 = 0.05, P2 = 0.95, P3 = 1.0;
+	// Weights
+	let W0 = 2.0, W1 = 1.0, W2 = 2.0, W3 = 1.0;
+
+	let b0 = (1 - t) ** 3;
+	let b1 = 3 * (1 - t) ** 2 * t;
+	let b2 = 3 * (1 - t) * t ** 2;
+	let b3 = t ** 3;
+	let w0 = b0 * W0;
+	let w1 = b1 * W1;
+	let w2 = b2 * W2;
+	let w3 = b3 * W3;
+
+	// Weighted sum of points
+	let numerator = w0 * P0 + w1 * P1 + w2 * P2 + w3 * P3;
+	let denominator = w0 + w1 + w2 + w3;
+
+	return numerator / denominator;
+}
+Blockbench.on('interpolate_keyframes', arg => {
+	if (Format.id != Config.format_id) return;
+	if (!arg.use_quaternions || !arg.t || arg.t == 1) return;
+	if (arg.keyframe_before.interpolation != 'catmullrom' || arg.keyframe_after.interpolation != 'catmullrom') return;
+	return {
+		t: weightedCubicBezier(arg.t)
+	}
+});
